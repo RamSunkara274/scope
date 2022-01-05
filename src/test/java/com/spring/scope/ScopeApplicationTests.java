d38f94c6d5ec66046c63e2065da673ba6431c4ed
RamSunkara274 <sunkararamarao2@gmail.com>
2022-01-05 21:00:18 +0530

    uploading new project name is scope

---
+++ src/test/java/com/spring/scope/ScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
