63d38e1559fcbd4ca08094fddab0b19c61440a61
RamSunkara274 <sunkararamarao2@gmail.com>
2022-01-06 20:04:56 +0530

    changes are adding to scope3 branch

--- src/main/java/com/spring/scope/ScopeApplication.java
+++ src/main/java/com/spring/scope/ScopeApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScopeApplication {
 
+	
+	//This class indicates the mail spring boot application
 	public static void main(String[] args) {
 		SpringApplication.run(ScopeApplication.class, args);
 	}
